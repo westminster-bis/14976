d14ded2d07b2bd4dd0383197e0968710bbfd9e20
00014976wiut <117806834+00014976wiut@users.noreply.github.com>
2023-06-02 08:16:56 -0700

    Create 14976_CSF_CW_REFERRAL.py
    Started code for Tic Tac Toe

---
+++ 14976_CSF_CW_REFERRAL.py
@@ -0,0 +1,20 @@
+#14976_TIC_TAC_TOE CW_REFERRAL CSF
+
+
+#First of all i create the board of the game
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+
+board_keys = []
+
+for key in theBoard:
+    board_keys.append(key)
+
+#Here i use first datatype - function. It is "printBoard". With this function i can print the updated board in the game.
+def printBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
