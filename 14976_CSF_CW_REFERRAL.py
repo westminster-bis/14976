143b58b56ca74ab3b5e6a3a7b4cf9d0df62b5799
00014976wiut <117806834+00014976wiut@users.noreply.github.com>
2023-06-02 08:18:23 -0700

    Update 14976_CSF_CW_REFERRAL.py
    Now, to check the winning condition, we will check a total of 8 conditions and whichever player has made the last move, we will declare that player as a winner. And if no one wins, we will declare tie

--- 14976_CSF_CW_REFERRAL.py
+++ 14976_CSF_CW_REFERRAL.py
@@ -38,3 +38,46 @@ def game():
         else:
             print("That place is already filled.\nMove to which place?")
             continue
+ # Now we game check if player X or O has won,for every move after 5 moves.
+        # Now, to check the winning condition, we will check a total of 8 conditions and whichever player has made the last move, we will declare that player as a winner. And if no one wins, we will declare tie 
+        if count >= 5:
+            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")                
+                break
+            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': # across the middle
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': # across the bottom
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break 
+            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break 
